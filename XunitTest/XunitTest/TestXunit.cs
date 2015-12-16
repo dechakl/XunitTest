1131ab5899112fe87ca6dc705ca47edb05ce3599
dechakl <decha@klnetwork.com>
2015-12-16 15:45:22 +0700

    new project

---
+++ XunitTest/XunitTest/TestXunit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XunitTest
+{
+    public class TestXunit
+    {
+
+        [Fact]
+        public void PassingTest()
+        {
+            var a = 4;
+            Assert.Equal(4, a);
+        }
+    }
+}
