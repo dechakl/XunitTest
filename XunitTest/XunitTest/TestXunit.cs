a4d393b7b040438410c46195fad1286e4ae3b04e
dechakl <decha@klnetwork.com>
2015-12-18 15:41:50 +0700

    add testcase with selenium

--- XunitTest/XunitTest/TestXunit.cs
+++ XunitTest/XunitTest/TestXunit.cs
@@ -4,11 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace XunitTest
 {
+   
     public class TestXunit
     {
+
+
+        [Fact]
+        public void TestSeleniumChrome()
+        {           
+            //using (IWebDriver driver = new ChromeDriver(@"dependencies"))
+            //{
+            //    driver.Navigate().GoToUrl("https://www.google.co.th");                     
+            //    driver.Quit();
+            //}
+
+            IWebDriver driver = new ChromeDriver(@"dependencies");
+            driver.Navigate().GoToUrl("https://www.google.co.th");
+            driver.Quit();
+            //Assert.Equals("https://www.google.co.th/", driver.Url);
+            Assert.Equal(5, 5);
+        }
 
         [Fact]
         public void PassingTest()
